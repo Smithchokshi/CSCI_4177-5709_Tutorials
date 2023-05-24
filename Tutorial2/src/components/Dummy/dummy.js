109f444ca6b8649fc8402a23e7116e09c8ac89a9
Smith Chokshi <sm714486@dal.ca>
2023-05-24 01:13:34 -0300

    Tutorial 2

---
+++ Tutorial2/src/components/Dummy/dummy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dummy = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'black', textAlign: 'center' }}>It works</h1>
+    </div>
+  );
+};
+
+export default Dummy;
